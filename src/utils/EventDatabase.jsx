350f427e29f8aa51c794fcb6087764bfb02bcfaa
Amit Kumar Deo <159774907+22AmitDeo@users.noreply.github.com>
2025-02-04 14:22:46 +0530

    Initial commit

---
+++ src/utils/EventDatabase.jsx
@@ -0,0 +1,39 @@
+export const eventList = [
+    {
+      id: 1,
+      heading: "BGMI",
+      date: { year: 2025, month: "Feb" },
+      location: "Online",
+      description:
+      "Grab your squad and lock in your game faces VTAPP’25 is here – a place where legends are made and chicken dinners are servedReady to dominate the map? Let the games begin",
+    img: "https://st1.techlusive.in/wp-content/uploads/2024/09/BGMI-3.4-update.jpg",
+    },
+    {
+      id: 2,
+      heading: "Uxcellence",
+      date: { year: 2024, month: "Nov" },
+      location: "Online",
+      description:
+        "Unleash your creativity and master the essentials of UI/UX! Join us for UXCELLENCE, a hands-on bootcamp covering everything from Figma basics to core UI/UX principles.",
+      img: "https://cdn.sanity.io/images/599r6htc/regionalized/5094051dac77593d0f0978bdcbabaf79e5bb855c-1080x1080.png?w=540&h=540&q=75&fit=max&auto=format",
+    },
+    {
+      id: 3,
+      heading: "Pydays",
+      date: { year: 2024, month: "Nov" },
+      location: "Online",
+      description:
+        "Join us for PyDays: An exciting online event hosted by the Team Next Nexus Club! Dive into the world of Python programming, enhance your skills, and connect with fellow learners. Whether you’re a beginner or looking to sharpen your expertise, there’s something for everyone. Don’t miss out on this opportunity to grow and innovate together!",
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSu9xFbA6COOd9Wq-koFEoAFD7wpFgbvdz6Q&s",
+    },
+    {
+      id: 4,
+      heading: "Freshers Connect",
+      date: { year: 2024, month: "Oct" },
+      location: "Newton Hall",
+      description:
+        "Bridging futures, sparking innovation, and building connections. Welcome to Nexus Connect: where freshers unite to explore technology and creativity!",
+      img: "https://t4.ftcdn.net/jpg/04/22/20/35/360_F_422203567_cHwMQWgXTrgN2CFGjkA4ia39IVnRfR57.jpg",
+    },  
+  ];
+  
